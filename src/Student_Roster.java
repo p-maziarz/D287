980b63ba40ee2ef1841cc51a025e9fe0df957bcd
Parker Maziarz <parkermaziarz@gmail.com>
2026-09-10 09:52:03 -0700

    added student roster

--- src/Student_Roster.java
+++ src/Student_Roster.java
@@ -1,7 +1,11 @@
 public class Student_Roster {
-    String[] students = {"1,John,Smith,John1989@gmail.com,20,88,79,59",
-            "2,Suzan,Erickson,Erickson_1990@gmailcom,19,91,72,85",
-            "3,Jack,Napoli,The_lawyer99yahoo.com,19,85,84,87",
-            "4,Erin,Black,Erin.black@comcast.net,22,91,98,82",
-            "5,[firstname],[lastname],[emailaddress],[age],[threetestscores]"};
+    String studentID;
+    String firstName;
+    String lastName;
+    String emailAddress;
+    int age;
+    String grades[];
+
+    public Student_Roster() {
+    }
 };
