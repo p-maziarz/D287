4d1870d3ce3d0344a71388bc7fd64847bcca3270
Parker Maziarz <parkermaziarz@gmail.com>
2026-09-11 07:53:18 -0700

    added instance variables, switched to student.java and added getters and setters

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,8 @@
 public class Student {
+    public static void main(String[] args) {
+        System.out.print(studentID, );
+    }
+
     private int studentID;
     private String firstName;
     private String lastName;
