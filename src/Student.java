6e59030448bff9983e2b2b3c6ecea91242ee921b
Parker Maziarz <parkermaziarz@gmail.com>
2026-09-11 07:33:17 -0700

    added instance variables, switched to student.java and added getters and setters

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,65 @@
 public class Student {
+    private int studentID;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private int age;
+    private int grades[];
+
+    public Student(int studentID, String firstName, String lastName, String emailAddress, int age, int[] grades) {
+        this.studentID = studentID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.age = age;
+        this.grades = grades;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int[] getGrades(int[] grades) {
+        return grades;
+    }
+
+    public void setGrades(int[] grades) {
+        this.grades = grades;
+    }
 }
--- src/Student_Roster.java
+++ src/Student_Roster.java
@@ -1,11 +1,3 @@
 public class Student_Roster {
-    String studentID;
-    String firstName;
-    String lastName;
-    String emailAddress;
-    int age;
-    String grades[];
 
-    public Student_Roster() {
-    }
 };
